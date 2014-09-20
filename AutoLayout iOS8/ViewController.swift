b5c6258d00a270f8e5c0ec3272cb3ca4353b5a01
Yi Wang <yiwang1982@Yis-MacBook-Air.local>
2014-09-20 16:00:13 -0400

    Initial Commit

---
+++ AutoLayout iOS8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout iOS8
+//
+//  Created by Yi Wang on 9/20/14.
+//  Copyright (c) 2014 Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AutoLayout iOS8Tests/AutoLayout_iOS8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AutoLayout_iOS8Tests.swift
+//  AutoLayout iOS8Tests
+//
+//  Created by Yi Wang on 9/20/14.
+//  Copyright (c) 2014 Yi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AutoLayout_iOS8Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
